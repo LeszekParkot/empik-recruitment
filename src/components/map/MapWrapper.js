117964a3b4671cf24a8debe31c628cb5cf6aa724
Leszek Parkot <cedro1812@gmail.com>
2020-09-10 17:38:53 +0200

    Refactoring.

--- src/components/map/MapWrapper.js
+++ src/components/map/MapWrapper.js
@@ -5,20 +5,22 @@ import mapData from "../../data/map-data";
 
 function MapWrapper() {
     return (
-        <div className="mapWrapper">
-            <svg fill="#7c7c7c" stroke="#ffffff" strokeWidth="2" viewBox="0 0 1000 948" xmlns="http://www.w3.org/2000/svg">
+        <section className="map">
+            <div className="container">
+                <svg stroke="#ffffff" viewBox="0 0 1000 948" xmlns="http://www.w3.org/2000/svg">
 
-                {
-                    mapData.map( province =>(
-                        <ProvinceArea
-                            shape={ province.shape }
-                            id={ province.id }
-                            key={ province.id } />
-                    ))
-                }
+                    {
+                        mapData.map( province =>(
+                            <ProvinceArea
+                                shape={ province.shape }
+                                id={ province.id }
+                                key={ province.id } />
+                        ))
+                    }
 
-            </svg>
-        </div>
+                </svg>
+            </div>
+        </section>
     );
 };
 
--- src/components/map/ProvinceArea.js
+++ src/components/map/ProvinceArea.js
@@ -4,6 +4,7 @@ import React from "react";
 function ProvinceArea(props) {
     return (
         <path
+            className="map__province-area"
             id={ props.id }
             d={ props.shape }>
         </path>
