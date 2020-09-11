4f0ac8988abe9fe57fe9b58122b24c4d04122065
Leszek Parkot <cedro1812@gmail.com>
2020-09-11 21:08:04 +0200

    Add context to App  and ProvinceArea.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import Map from "./components/map/MapWrapper";
+import Slider from "./components/slider/Slider";
+import AppContext from "./context";
 
+class App extends React.Component{
 
-function App() {
-    return (
-        <Map />
-    );
+    state = {
+        sliderOff: true
+    }
+
+    testFunction = (e) => {
+        this.setState({ sliderOff: false });
+        console.log(e.target.id);
+    }
+
+    testFunction2 = () => {
+        console.log("siema");
+    }
+
+    render(){
+        console.log(this.state.sliderOff);
+
+        return (
+            <AppContext.Provider value={{
+                testFunction: this.testFunction,
+                testFunction2: this.testFunction2}}>
+                <Map />
+                {
+                    this.state.sliderOff || <Slider />
+                }
+            </AppContext.Provider>
+        );
+    }
+
 }
 
 export default App;
--- src/components/map/ProvinceArea.js
+++ src/components/map/ProvinceArea.js
@@ -1,13 +1,39 @@
 import React from "react";
-
+import AppContext from "../../context";
 
 function ProvinceArea(props) {
     return (
-        <path
-            className="map__province-area"
-            id={ props.id }
-            d={ props.shape }>
-        </path>
+        // <AppContext.Consumer>
+        // {(context) => (
+        //     <path
+        //         className="map__province-area"
+        //         onClick={ context }
+        //         id={ props.id }
+        //         d={ props.shape }>
+        //     </path>
+        // )}
+        // </AppContext.Consumer>
+
+
+        <AppContext.Consumer>
+
+        {(context) => {
+            // console.log(context);
+            return(
+                <path
+                    className="map__province-area"
+                    onClick={ context.testFunction }
+                    id={ props.id }
+                    d={ props.shape }>
+                </path>
+            )
+        }}
+
+
+
+
+        </AppContext.Consumer>
+
     )
 }
 
