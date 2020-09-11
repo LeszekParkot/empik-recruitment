f506593680efab8bd6d27d70cd19b8155703495b
Leszek Parkot <cedro1812@gmail.com>
2020-09-11 21:15:19 +0200

    Add slider component.

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import AppContext from "../../context";
+
+
+
+function Slider(){
+    return(
+        <section className="slider">
+            <div className="container">
+                <p>SLIDER</p>
+            </div>
+        </section>
+    )
+}
+
+export default Slider;
